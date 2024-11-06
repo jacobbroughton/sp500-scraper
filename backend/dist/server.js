61c37ea9f6fe97b1e21e6926558c5506ec48226b
Jacob Broughton <jlbroughton88@gmail.com>
2024-11-06 13:00:33 -0500

    updated tsconfig

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -22,15 +22,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -42,33 +33,30 @@ const app = (0, express_1.default)();
 app.use(cors());
 const hostname = "127.0.0.1";
 const port = 4000;
-function scrape() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch("https://www.slickcharts.com/sp500");
-        const htmlStr = yield response.text();
-        const $ = cheerio.load(htmlStr);
-        const elements = [];
-        $("table:first > tbody > tr").each((i, el) => {
-            var _a, _b;
-            const htmlString = $(el).html();
-            const splitByTd = htmlString === null || htmlString === void 0 ? void 0 : htmlString.split('">');
-            if (!splitByTd) {
-                return;
-            }
-            const matchCompany = /(\b[\w\d\s]+.+(?=<\/a))/g;
-            const matchSymbol = /(\b[\w\d\s]+(?=<\/a))/g;
-            const company = (_a = splitByTd[2].match(matchCompany)) === null || _a === void 0 ? void 0 : _a[0].replace("&amp;", "&");
-            const symbol = (_b = splitByTd[3].match(matchSymbol)) === null || _b === void 0 ? void 0 : _b[0];
-            elements.push({ rank: i + 1, symbol, company });
-        });
-        return elements;
+async function scrape() {
+    const response = await fetch("https://www.slickcharts.com/sp500");
+    const htmlStr = await response.text();
+    const $ = cheerio.load(htmlStr);
+    const elements = [];
+    $("table:first > tbody > tr").each((i, el) => {
+        const htmlString = $(el).html();
+        const splitByTd = htmlString?.split('">');
+        if (!splitByTd) {
+            return;
+        }
+        const matchCompany = /(\b[\w\d\s]+.+(?=<\/a))/g;
+        const matchSymbol = /(\b[\w\d\s]+(?=<\/a))/g;
+        const company = splitByTd[2].match(matchCompany)?.[0].replace("&amp;", "&");
+        const symbol = splitByTd[3].match(matchSymbol)?.[0];
+        elements.push({ rank: i + 1, symbol, company });
     });
+    return elements;
 }
-app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const data = yield scrape();
+app.get("/", async (req, res) => {
+    const data = await scrape();
     console.log(data.length);
     res.send(JSON.stringify(data));
-}));
+});
 app.listen(port, () => {
     console.log(`Server is running on http://${hostname}:${port}`);
 });
