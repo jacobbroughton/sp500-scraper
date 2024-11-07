31975ed74e47a37d857a85cdfa28408d6767c2b6
Jacob Broughton <jlbroughton88@gmail.com>
2024-11-07 09:19:17 -0500

    removed js file at root, recompiled

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,42 +1,20 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cheerio = __importStar(require("cheerio"));
-const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+const express = require("express");
+const cheerio = require("cheerio");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fs = require("fs");
+dotenv.config();
+const app = express();
+app.use(cors());
 const hostname = "127.0.0.1";
 const port = 4000;
 async function scrape() {
     const response = await fetch("https://www.slickcharts.com/sp500");
     const htmlStr = await response.text();
     const $ = cheerio.load(htmlStr);
+    console.log("scraping");
     const elements = [];
     $("table:first > tbody > tr").each((i, el) => {
         const htmlString = $(el).html();
@@ -53,10 +31,53 @@ async function scrape() {
     return elements;
 }
 app.get("/", async (req, res) => {
-    const data = await scrape();
-    console.log(data.length);
-    res.send(JSON.stringify(data));
+    let jsonData = "";
+    fs.readFile("data.json", "utf-8", async (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        else {
+            const parsedData = JSON.parse(data);
+            const oneDayMS = 1000 * 60 * 60 * 24;
+            const timeNeededToFetchMS = oneDayMS * 5;
+            const timeSinceLastFetchedMS = new Date().getTime() - new Date(parsedData.lastFetched).getTime();
+            if (timeSinceLastFetchedMS > timeNeededToFetchMS) {
+                const data = await scrape();
+                jsonData = JSON.stringify({
+                    lastFetched: new Date().toLocaleString(),
+                    companies: data,
+                }, null, 2);
+                createNewFile(jsonData);
+                res.send(jsonData);
+            }
+            else {
+                jsonData = data;
+                res.send(jsonData);
+            }
+        }
+    });
 });
+function createNewFile(jsonData) {
+    fs.unlink("data.json", (err) => {
+        if (err) {
+            console.error("something happened when unlinking the file");
+        }
+        else {
+            console.log("File was deleted");
+        }
+    });
+    fs.writeFile("data.json", jsonData, (err) => {
+        if (err) {
+            console.error("something happened when saving the file");
+        }
+        else {
+            console.log("File was created");
+        }
+    });
+}
 app.listen(port, () => {
-    console.log(`Server is running on http://${hostname}:${port}`);
+    console.log(`Server is running on ${process.env.NODE_ENV === "development"
+        ? `http://${hostname}:${port}`
+        : "https://sp500-scraper.onrender.com/"}`);
 });
+//# sourceMappingURL=server.js.map
