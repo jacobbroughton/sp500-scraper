b8c72731ce109bffb9b8d47a2d610eb0d1c7620d
Jacob Broughton <jlbroughton88@gmail.com>
2024-11-08 08:28:07 -0500

    testing previous build...

--- frontend/src/hooks/useScrapedData.ts
+++ frontend/src/hooks/useScrapedData.ts
@@ -19,9 +19,9 @@ export function useScrapedData(): {
       setLoading(true);
 
       const response = await fetch(
-        import.meta.env.DEV
-          ? "http://localhost:4000"
-          : "https://sp500-scraper.onrender.com/"
+        import.meta.env.PROD
+          ? "https://sp500-scraper.onrender.com/"
+          : "http://localhost:4000"
       );
 
       if (!response.ok) throw new Error("Something happened");
