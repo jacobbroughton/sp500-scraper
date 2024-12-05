4541598b070bfb46da541b1d71c49d29a797cc80
Jacob Broughton <jlbroughton88@gmail.com>
2024-12-05 15:38:28 -0500

    fixed with template strings and some func changes

--- dist/server.js
+++ dist/server.js
@@ -35,6 +35,15 @@ app.get("/", async (req, res) => {
     fs.readFile("data.json", "utf-8", async (err, data) => {
         if (err) {
             console.error(err);
+            throw err;
+        }
+        if (!data) {
+            const data = await scrape();
+            jsonData = JSON.stringify({
+                lastFetched: new Date().toLocaleString(),
+                companies: data,
+            }, null, 2);
+            createNewFile(jsonData);
         }
         else {
             const parsedData = JSON.parse(data);
@@ -47,18 +56,139 @@ app.get("/", async (req, res) => {
                     lastFetched: new Date().toLocaleString(),
                     companies: data,
                 }, null, 2);
-                createNewFile(jsonData);
-                res.send(jsonData);
             }
             else {
                 jsonData = data;
-                res.send(jsonData);
             }
         }
+        const parsedJsonData = JSON.parse(jsonData);
+        // res.send(jsonData)
+        // return
+        res.send(`
+      <html>
+      <head>
+        <style>
+          :root {
+            font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+            line-height: 1.5;
+            font-weight: 400;
+
+            color-scheme: light dark;
+            color: rgba(255, 255, 255, 0.87);
+            background-color: #242424;
+
+            font-synthesis: none;
+            text-rendering: optimizeLegibility;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+
+          * {
+            padding: 0;
+            margin: 0;
+          }
+
+          main {
+            margin: 0 auto;
+            max-width: 520px;
+            padding-bottom: 50px;
+          }
+
+          main > div.footnotes {
+            margin-bottom: 10px;
+          }
+
+          main > div.footnotes > p {
+            font-size: 0.875rem;
+          }
+
+          main > h1 {
+            margin: 30px 0 10px 0;
+          }
+
+          main > div.error {
+            border: 1px solid crimson;
+            border-radius: 5px;
+            padding: 10px;
+            color: crimson;
+            font-size: 0.875rem;
+          }
+
+          main > div.error > p:first-child {
+            font-size: 1rem;
+          }
+
+          main > div.table-parent,
+          main > div.loading {
+            border: 1px solid #555;
+            border-radius: 5px;
+            padding: 10px;
+          }
+
+          main > div.table-parent > table {
+          }
+
+          main > div.table-parent > table > thead > tr > th,
+          main > div.table-parent > table > tbody > tr > td {
+            text-align: start;
+            padding: 3px 5px;
+          }
+
+          main > div.table-parent > table > thead > tr > th {
+            padding-top: 0;
+          }
+
+          main > div.table-parent > table > tbody > tr > td.rank {
+            color: grey;
+          }
+
+          main > div.loading {
+            display: flex;
+            gap: 5px;
+            flex-direction: column;
+          }
+
+          main > div.loading > ul {
+            list-style: none;
+          }
+        </style>
+      </head>
+        <body>
+
+           <main>
+            <h1>S&P 500 Scraper</h1>
+            <div class="footnotes">
+              <p>Data updates every 5 days</p>
+              <p>Last scrape: ${parsedJsonData.lastFetched}</p>
+            </div>
+            
+            <div class="table-parent">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Rank</th> <th>Symbol</th> <th>Company</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  ${parsedJsonData.companies.map((company) => `
+                      <tr>
+                        <td class="rank">${company.rank}</td>
+                        <td class="symbol">${company.symbol}</td>
+                        <td class="company">${company.company}</td>
+                      </tr>
+                      `).join("")}
+                </tbody>
+              </table>
+            </div>
+            
+          </main>
+        </body>
+      </html>
+    `);
     });
 });
 function createNewFile(jsonData) {
-    fs.unlink("data.json", (err) => {
+    fs.writeFile("data.json", "", (err) => {
         if (err) {
             console.error("something happened when unlinking the file");
         }
--- server.ts
+++ server.ts
@@ -46,9 +46,25 @@ async function scrape() {
 
 app.get("/", async (req: Request, res: Response) => {
   let jsonData: string = "";
+
   fs.readFile("data.json", "utf-8", async (err, data) => {
     if (err) {
       console.error(err);
+      throw err;
+    }
+    if (!data) {
+      const data = await scrape();
+
+      jsonData = JSON.stringify(
+        {
+          lastFetched: new Date().toLocaleString(),
+          companies: data,
+        },
+        null,
+        2
+      );
+
+      createNewFile(jsonData);
     } else {
       const parsedData: {
         lastFetched: string;
@@ -71,21 +87,147 @@ app.get("/", async (req: Request, res: Response) => {
           null,
           2
         );
-
-        createNewFile(jsonData);
-
-        res.send(jsonData);
       } else {
         jsonData = data;
-
-        res.send(jsonData);
       }
     }
+
+    const parsedJsonData: {
+      lastFetched: string;
+      companies: [{ rank: string; symbol: string; company: string }];
+    } = JSON.parse(jsonData);
+
+    // res.send(jsonData)
+    // return
+
+    res.send(`
+      <html>
+      <head>
+        <style>
+          :root {
+            font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+            line-height: 1.5;
+            font-weight: 400;
+
+            color-scheme: light dark;
+            color: rgba(255, 255, 255, 0.87);
+            background-color: #242424;
+
+            font-synthesis: none;
+            text-rendering: optimizeLegibility;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+
+          * {
+            padding: 0;
+            margin: 0;
+          }
+
+          main {
+            margin: 0 auto;
+            max-width: 520px;
+            padding-bottom: 50px;
+          }
+
+          main > div.footnotes {
+            margin-bottom: 10px;
+          }
+
+          main > div.footnotes > p {
+            font-size: 0.875rem;
+          }
+
+          main > h1 {
+            margin: 30px 0 10px 0;
+          }
+
+          main > div.error {
+            border: 1px solid crimson;
+            border-radius: 5px;
+            padding: 10px;
+            color: crimson;
+            font-size: 0.875rem;
+          }
+
+          main > div.error > p:first-child {
+            font-size: 1rem;
+          }
+
+          main > div.table-parent,
+          main > div.loading {
+            border: 1px solid #555;
+            border-radius: 5px;
+            padding: 10px;
+          }
+
+          main > div.table-parent > table {
+          }
+
+          main > div.table-parent > table > thead > tr > th,
+          main > div.table-parent > table > tbody > tr > td {
+            text-align: start;
+            padding: 3px 5px;
+          }
+
+          main > div.table-parent > table > thead > tr > th {
+            padding-top: 0;
+          }
+
+          main > div.table-parent > table > tbody > tr > td.rank {
+            color: grey;
+          }
+
+          main > div.loading {
+            display: flex;
+            gap: 5px;
+            flex-direction: column;
+          }
+
+          main > div.loading > ul {
+            list-style: none;
+          }
+        </style>
+      </head>
+        <body>
+
+           <main>
+            <h1>S&P 500 Scraper</h1>
+            <div class="footnotes">
+              <p>Data updates every 5 days</p>
+              <p>Last scrape: ${parsedJsonData.lastFetched}</p>
+            </div>
+            
+            <div class="table-parent">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Rank</th> <th>Symbol</th> <th>Company</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  ${parsedJsonData.companies.map(
+                    (company) => `
+                      <tr>
+                        <td class="rank">${company.rank}</td>
+                        <td class="symbol">${company.symbol}</td>
+                        <td class="company">${company.company}</td>
+                      </tr>
+                      `
+                  ).join("")}
+                </tbody>
+              </table>
+            </div>
+            
+          </main>
+        </body>
+      </html>
+    `);
   });
 });
 
 function createNewFile(jsonData: string) {
-  fs.unlink("data.json", (err) => {
+  fs.writeFile("data.json", "", (err) => {
     if (err) {
       console.error("something happened when unlinking the file");
     } else {
